66b376e99ec866f7bc12e83aa1a27e6a2ec103cb
PhamDucDatCN2301CLCB <hhakio1234@gmail.com>
2026-06-19 10:34:44 +0700

    Implement GET accounts API

--- database/schema.sql
+++ database/schema.sql
@@ -46,7 +46,6 @@ CREATE TABLE IF NOT EXISTS products (
 -- Bảng Tài Khoản (accounts)
 -- Dùng cho đăng ký, đăng nhập và phân quyền
 -- =============================================
-
 CREATE TABLE IF NOT EXISTS accounts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
